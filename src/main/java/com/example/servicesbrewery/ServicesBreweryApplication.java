9e9513ecc01e422d92f2bfc369c723f91f7743c1
Yash Srivastava <yashsrivastava@hotmail.com>
2023-03-18 07:30:23 +0530

    Initial commit

---
+++ src/main/java/com/example/servicesbrewery/ServicesBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicesbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servicesbrewery/ServicesBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicesbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
